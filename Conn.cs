ff95f8a3ce25631add52a90749e7844ea31e08e8
SmartFg <mrTangFg@163.com>
2022-09-13 14:19:59 +0800

    Added dependent files and some necessary comments

--- Conn.cs
+++ Conn.cs
@@ -8,12 +8,12 @@ using MySql.Data.MySqlClient;
 namespace test1
 {
     /// <summary>
-    /// 用于连接数据库，防止代码重复
+    /// 用于连接数据库，防止代码重复 
     /// </summary>
     class MysqlConn
     {
         //定义链接字符串
-        string connString = "server=127.0.0.1;port=3306;user=root;password=123;database=test;Charset=utf8;";
+        string connString = "server=127.0.0.1;port=3306;user=root;password=123;database=test;Charset=utf8;"; //Modify it to your own database. Please ensure that the required tables are created
 
         //创建连接对象,创建类时自动创建SQL连接
         public MySqlConnection Conn;
